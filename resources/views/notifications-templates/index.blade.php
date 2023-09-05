f7229d2d3978348f681f5e0105a88e8251eb069c
marious <mohamedraya410@gmail.com>
2023-09-05 16:07:17 +0300

    fix-issues-to-match-new-version-of-tomato

--- resources/views/notifications-templates/index.blade.php
+++ resources/views/notifications-templates/index.blade.php
@@ -1,38 +1,35 @@
 <x-tomato-admin-layout>
-    <x-slot name="header">
+    <x-slot:header>
         {{ trans('tomato-notifications::global.templates.title') }}
-    </x-slot>
-    <x-slot name="headerBody">
-        <Link modal href="/admin/notifications-templates/create" class="filament-button inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset dark:focus:ring-offset-0 min-h-[2.25rem] px-4 text-sm text-white shadow focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 filament-page-button-action">
+    </x-slot:header>
+    <x-slot:buttons>
+        <x-tomato-admin-button :modal="true" href="/admin/notifications-templates/create" type="link">
             {{trans('tomato-admin::global.crud.create-new')}} {{ trans('tomato-notifications::global.templates.single') }}
-        </Link>
-    </x-slot>
+        </x-tomato-admin-button>
+    </x-slot:buttons>
 
-    <div class="pb-12" v-cloak>
-        <div class="mx-auto">
-            @if(\TomatoPHP\TomatoNotifications\Models\NotificationsTemplate::count())
+    <div class="pb-12">
             <x-splade-table :for="$table" striped>
                 <x-splade-cell actions>
                     <div class="flex justify-start">
-                        <Link href="/admin/notifications-templates/{{ $item->id }}/send" class="px-2 text-green-500" confirm>
+                        <x-splade-link href="/admin/notifications-templates/{{ $item->id }}/send" class="px-2
+                        text-green-500" confirm>
                             <div class="flex justify-start space-x-2">
                                 <x-heroicon-s-paper-airplane class="h-4 w-4 ltr:mr-2 rtl:ml-2"/>
-                                <span>{{trans('tomato-notifications::global.templates.send')}}</span>
+                                {{ __('Send') }}
                             </div>
-                        </Link>
-                        <Link href="/admin/notifications-templates/{{ $item->id }}" class="px-2 text-blue-500" modal>
-                            <div class="flex justify-start space-x-2">
-                                <x-heroicon-s-eye class="h-4 w-4 ltr:mr-2 rtl:ml-2"/>
-                                <span>{{trans('tomato-admin::global.crud.view')}}</span>
-                            </div>
-                        </Link>
-                        <Link href="/admin/notifications-templates/{{ $item->id }}/edit" class="px-2 text-yellow-400" modal>
-                            <div class="flex justify-start space-x-2">
-                                <x-heroicon-s-pencil class="h-4 w-4 ltr:mr-2 rtl:ml-2"/>
-                                <span>{{trans('tomato-admin::global.crud.edit')}}</span>
-                            </div>
-                        </Link>
-                        <Link href="/admin/notifications-templates/{{ $item->id }}"
+                        </x-splade-link>
+
+
+                        <x-tomato-admin-button success type="icon" title="{{trans('tomato-admin::global.crud.view')}}" modal :href="route('admin.notifications-templates.show', $item->id)">
+                            <x-heroicon-s-eye class="h-4 w-4 ltr:mr-2 rtl:ml-2"/>
+                        </x-tomato-admin-button>
+
+                        <x-tomato-admin-button warning type="icon" title="{{trans('tomato-admin::global.crud.edit')}}" modal :href="route('admin.notifications-templates.edit', $item->id)">
+                            <x-heroicon-s-pencil class="h-4 w-4 ltr:mr-2 rtl:ml-2"/>
+                        </x-tomato-admin-button>
+
+                        <x-splade-link href="/admin/notifications-templates/{{ $item->id }}"
                               confirm="{{trans('tomato-admin::global.crud.delete-confirm')}}"
                               confirm-text="{{trans('tomato-admin::global.crud.delete-confirm-text')}}"
                               confirm-button="{{trans('tomato-admin::global.crud.delete-confirm-button')}}"
@@ -43,40 +40,10 @@
                         >
                             <div class="flex justify-start space-x-2">
                                 <x-heroicon-s-trash class="h-4 w-4 ltr:mr-2 rtl:ml-2"/>
-                                <span>{{trans('tomato-admin::global.crud.delete')}}</span>
                             </div>
-                        </Link>
+                        </x-splade-link>
                     </div>
                 </x-splade-cell>
             </x-splade-table>
-            @else
-                <div class="relative text-center">
-                    <div class="flex items-center justify-center">
-                        <div
-                            class="flex flex-col items-center justify-center flex-1 p-6 mx-auto space-y-6 text-center bg-white filament-tables-empty-state dark:bg-gray-800 rounded-lg shadow-sm">
-                            <div
-                                class="flex items-center justify-center w-16 h-16 rounded-full text-primary-500 bg-primary-50 dark:bg-gray-700">
-                                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-                                     stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
-                                </svg>
-                            </div>
-
-                            <div class="max-w-md space-y-1">
-                                <h2 class="text-xl font-bold tracking-tight filament-tables-empty-state-heading dark:text-white">
-                                    {{ trans('tomato-admin::global.empty') }}
-                                </h2>
-
-                                <p
-                                    class="text-sm font-medium text-gray-500 whitespace-normal filament-tables-empty-state-description dark:text-gray-400">
-
-                                </p>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            @endif
         </div>
-    </div>
 </x-tomato-admin-layout>
